39b7eee8004cd340269a2c96c4d12617b9b5f86b
Stoma655 <stoma66555@gmail.com>
2019-07-15 00:22:35 +0300

    up5

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+require('dotenv').config();
 
 app.use(express.static('public'));
 
@@ -27,6 +28,6 @@ io.on('connection', function(socket){
 
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(process.env.PORT , function(){
+    console.log('listening on *:'+ process.env.PORT +'');
   });

20e2d18de1827f470786610a7f081e2ca3cffdcd
Stoma655 <stoma66555@gmail.com>
2019-07-14 22:50:19 +0300

    сделай лучше

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('a user connected ' + socket.id);
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+
+    socket.on('spavned', function(){
+        // console.log('заспавнили бота')
+        var sid = socket.id;
+        io.emit('spavned on area', sid);
+    });
+
+    socket.on('set coords', function(data) {
+        io.emit('get enemy coords', data);
+    });
+
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+  });
